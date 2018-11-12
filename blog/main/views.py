57b81f297028f16dc1c034d5ad147c9374443261
qast223322 <a0982417737@gmail.com>
2018-11-12 15:08:57 +0800

    1112

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def main(request):
     '''
-    Show 'Hello world!' in the main page
+    Render the main page
     '''
-    return HttpResponse('Hello world!')
+    context = {'like':'Django 很棒'}
+    return render(request, 'main/main.html', context)
+
+def about(request):
+    '''
+    Render the about page
+    '''
+    return render(request, 'main/about.html')
